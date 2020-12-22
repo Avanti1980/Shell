39930a86ddd536003ca96b3c36b06fb0bd3cfe08
Avanti <murongxixi@gmail.com>
2020-12-22 12:45:38 +0800

    update

--- count-pacman-update.sh
+++ count-pacman-update.sh
@@ -1,1 +1,1 @@
-echo $(head -n1 /var/log/pacman.log | cut -d " " -f 1,2) 以来一共滚了 $(grep -c "full system upgrade" /var/log/pacman.log) 次
+echo 自 $(head -n1 /var/log/pacman.log | cut -d '[' -f 2 | cut -d ']' -f 1) 以来 $(head -n1 /var/log/pacman.log | cut -d '[' -f 3 | cut -d ']' -f 1) 一共滚了 $(grep -c "full system upgrade" /var/log/pacman.log) 次
